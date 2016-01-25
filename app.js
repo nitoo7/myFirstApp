d8da1ac43917657b06d1d652560f721f76ca272c
nithin <nithint@qburst.com>
2016-01-25 17:03:30 +0530

    connection to mongodb success

--- app.js
+++ app.js
@@ -1,18 +1,28 @@
+
 var express = require('express');
 var app = express();
 const bluebird = require('bluebird');
 const _ = require('lodash');
-var MongoClient = require('mongodb').MongoClient;
+
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/example');
+var Schema = mongoose.Schema;
+var movieSchema = new Schema({
+  title: String,
+  year: Number,
+  director: String,
+  rating: String
+});
+var Movie = mongoose.model('movie', movieSchema);
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
 app.get('/details', function (req, res) {
-  var desc = {"title":"abc","desc":"abc"}
-  res.send(desc);
+  Movie.findOne({}).exec().then(function(data){return res.send(data)})
 });
 
-app.listen(process.env.PORT, function () {
+app.listen(process.env.PORT || 3000, function () {
   console.log('Example app listening on port 3000!');
 });
