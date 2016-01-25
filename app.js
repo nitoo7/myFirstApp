15a5772e404bd411c586fb2376b9b9e6174717d5
nithin <nithint@qburst.com>
2016-01-25 15:37:12 +0530

    further changes

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 var express = require('express');
 var app = express();
+const bluebird = require('bluebird');
+const _ = require('lodash');
+var MongoClient = require('mongodb').MongoClient;
 
 app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+app.get('/details', function (req, res) {
+  var desc = {"title":"abc","desc":"abc"}
   res.send('Hello World!');
 });
 
