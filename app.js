be7a7d0f6d52041095746b2c2dbcd980f23f0d6c
nithin <nithint@qburst.com>
2016-01-27 15:03:09 +0530

    ads

--- app.js
+++ app.js
@@ -4,41 +4,26 @@ var app = express();
 const bluebird = require('bluebird');
 const _ = require('lodash');
 
-//var mongoose = require('mongoose');
+var mongoose = require('mongoose');
 //mongoose.connect('mongodb://localhost:27017/example');
-//mongoose.connect('mongodb://example:example@ds053312.mongolab.com:53312/example);
-//var Schema = mongoose.Schema;
-//var movieSchema = new Schema({
-//  title: String,
-//  year: Number,
-//  director: String,
-//  rating: String
-//});
-//var Movie = mongoose.model('movie', movieSchema);
+mongoose.connect('mongodb://nitoo7:manchester007@ds051585.mongolab.com:51585/sampletestdb');
 
-//var first = new Movie({
-//  title: 'asdfasdd',
-//  year: 123,
-//  director: 'asdasd',
-//  rating: 'asdasd'
-//})
+var Schema = mongoose.Schema;
+var movieSchema = new Schema({
+ title: String,
+  year: Number,
+  director: String,
+  rating: String
+});
+var Movie = mongoose.model('movie', movieSchema);
 
-//first.save(function(err, data) {
-//  if (err) return console.error(err);
-//  console.log('***->'data);
-//});
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
 app.get('/details', function (req, res) {
-var resp = {"title": "JAWS",
-    "year": "1951",
-    "director": "Steven speilberg",
-    "rating": "PG"}
-  //Movie.findOne({}).exec().then(function(data){return res.send(data)})
-res.send(resp);
+  Movie.findOne({}).exec().then(function(data){return res.send(data)})
 });
 
 app.listen(process.env.PORT || 3000, function () {
