263186ec9ac3c6854f73501a1ab760937062151c
nithin <nithint@qburst.com>
2016-01-27 11:48:10 +0530

    sample

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ const bluebird = require('bluebird');
 const _ = require('lodash');
 
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://example:example@ds053312.mongolab.com:53312/todolist');
+//mongoose.connect('mongodb://localhost:27017/example');
+mongoose.connect('mongodb://example:example@ds053312.mongolab.com:53312/example);
 var Schema = mongoose.Schema;
 var movieSchema = new Schema({
   title: String,
@@ -15,12 +16,29 @@ var movieSchema = new Schema({
 });
 var Movie = mongoose.model('movie', movieSchema);
 
+var first = new Movie({
+  title: 'asdfasdd',
+  year: 123,
+  director: 'asdasd',
+  rating: 'asdasd'
+})
+
+first.save(function(err, data) {
+  if (err) return console.error(err);
+  console.log('***->'data);
+});
+
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
 app.get('/details', function (req, res) {
-  Movie.findOne({}).exec().then(function(data){return res.send(data)})
+var res = {"title": "JAWS",
+    "year": "1951",
+    "director": "Steven speilberg",
+    "rating": "PG"}
+  //Movie.findOne({}).exec().then(function(data){return res.send(data)})
+return.send(res);
 });
 
 app.listen(process.env.PORT || 3000, function () {
